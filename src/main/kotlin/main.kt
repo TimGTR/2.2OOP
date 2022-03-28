46e213be1a2deca60bb7b1a3c9768095eeacb1fd
Timur Garipov <tr.garipov@yandex.ru>
2022-03-28 20:14:05 +0300

    +tests

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,14 @@
 
 
 fun main() {
+    val post = WallService
+    post.add(Post(1, 1, 1, 1, 1, "1",1, 1, true ,1,1,1,
+        1, 1, "1", 1, true, true, true, true,
+        true, true, true, 1 ))
 
 }
 data class Post(
-    val id: Int,
+    var id: Int,
     val ownerId: Int,
     val fromId: Int,
     val createdBy: Int,
@@ -28,12 +32,27 @@ data class Post(
     val isFavorite: Boolean,
     val donut: Any,
     val postponedId: Int)
+
+
 object WallService {
     private var posts = emptyArray<Post>()
+    var id = 0
 
     fun add (post: Post): Post {
         posts += post
+        id += 1
         return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        return if (post.id == id) {
+            val post = post.copy(ownerId = post.ownerId + 1, fromId = post.fromId + 1, createdBy = post.createdBy + 1,
+                replyOwnerId = post.replyOwnerId + 1,replyPostId = post.replyPostId + 1, signerId = post.signerId + 1,
+                postponedId = post.postponedId + 1)
+            true
+        } else {
+            false
+        }
     }
 }
 
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,38 @@
+
+import WallService.id
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        val service = WallService
+        service.add(Post(1, 1, 1, 1, 1, "1",1, 1, true ,1,1,1,
+        1, 1, "1", 1, true, true, true, true,
+            true, true, true, 1 ))
+
+        val result = service.id
+
+        assertNotEquals(0, result)
+    }
+
+    @Test
+    fun update() {
+        val service = WallService
+        service.add(Post(1, 1, 1, 1, 1, "1",1, 1, true ,1,1,1,
+            1, 1, "1", 1, true, true, true, true,
+            true, true, true, 1 ))
+        service.add(Post(2, 1, 1, 1, 1, "1",1, 1, true ,1,1,1,
+            1, 1, "1", 1, true, true, true, true,
+            true, true, true, 1 ))
+        val update = Post(2, 1, 1, 1, 1, "1",1, 1, true ,1,1,1,
+            1, 1, "1", 1, true, true, true, true,
+            true, true, true, 1)
+        val result = service.update(update)
+        assertTrue(result)
+    }
+
+}
