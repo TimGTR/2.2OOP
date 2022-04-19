5b49039a94df23b6efca79a6f258c74a6beeb321
Timur Garipov <tr.garipov@yandex.ru>
2022-04-19 22:01:25 +0300

    corr

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,64 +7,9 @@ fun main() {
         true, true, true, 1 ))
     print(post1.id)
 }
-data class Post(
-    var id: Int,
-    val ownerId: Int,
-    val fromId: Int,
-    val createdBy: Int,
-    val date: Int,
-    val text: String,
-    val replyOwnerId: Int,
-    val replyPostId: Int,
-    val friendsOnly: Boolean,
-    val comments: Any,
-    val copyright: Any,
-    val likes: Any,
-    val reposts: Any,
-    val views: Any,
-    val postType: String,
-    val signerId: Int,
-    val canPin: Boolean,
-    val canDelete: Boolean,
-    val canEdit: Boolean,
-    val isPinned: Boolean,
-    val markedAsAdds: Boolean,
-    val isFavorite: Boolean,
-    val donut: Any,
-    val postponedId: Int)
 
 
-object WallService {
-    private var posts = emptyArray<Post>()
-    var postId = 1
 
-    fun add(post: Post): Post {
-        post.id = postId
-        posts += post
-        postId++
-        return posts.last()
-    }
-
-    fun update(post: Post): Boolean {
-        for ((index, post1) in posts.withIndex()) {
-            return if (post.id == post1.id) {
-                posts[index] = post.copy(
-                    ownerId = post.ownerId + 1,
-                    fromId = post.fromId + 1,
-                    createdBy = post.createdBy + 1,
-                    replyOwnerId = post.replyOwnerId + 1,
-                    replyPostId = post.replyPostId + 1,
-                    signerId = post.signerId + 1,
-                    postponedId = post.postponedId + 1
-                )
-                true
-            } else {
-                false
-            }
-        }
-        return false
-    }
-}
 
 
 
